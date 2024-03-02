8c593ad1f94373796bbab55928a9b5929f910dbb
Artem Hvozdov <113240736+ArtemHvozdov@users.noreply.github.com>
2024-03-02 22:07:24 +0200

    add tittle font, add hover effect icons social media in footer (#2)
    Co-authored-by: ArtemHvozdov <artemhvozdov@gmail.com>

--- src/layout/footer/footer-glass/footer-glass.jsx
+++ src/layout/footer/footer-glass/footer-glass.jsx
@@ -4,16 +4,21 @@ import Copyright from '../copyright/copyright';
 
 function FooterGlass() {
     const styles = {
+        footer__glass: {
             position: "relative",
             background: "var(--footer-glass-bg-color)",
             backdropFilter: "blur(30px)",
             borderTopLeftRadius: "36px",
-            borderRopRightRadius: "36px" 
+            borderRopRightRadius: "36px"
+        },
+        footer__glass_inner: {
+            padding: "30px 0px 0px 0px"
+        }
     }
     return (
-        <div className="footer__glass" style={styles}>
+        <div className="footer__glass" style={styles.footer__glass}>
             <div className="container">
-                <div className="footer__glas-inner">
+                <div className="footer__glas-inner" style={styles.footer__glass_inner}>
                     <div className="footer__glass-logo">
                         <img src={logoFooterImg} alt="" />
                     </div>
